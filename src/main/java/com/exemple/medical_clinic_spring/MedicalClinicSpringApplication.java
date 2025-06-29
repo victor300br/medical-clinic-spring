0df1db98f07a08724df3fb49381458766d533266
victor300br <victorbora52@gmail.com>
2025-06-29 13:48:55 -0300

    initial commit

---
+++ src/main/java/com/exemple/medical_clinic_spring/MedicalClinicSpringApplication.java
@@ -0,0 +1,13 @@
+package com.exemple.medical_clinic_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalClinicSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalClinicSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exemple/medical_clinic_spring/MedicalClinicSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemple.medical_clinic_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalClinicSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
